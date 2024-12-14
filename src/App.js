c53900fbecadb9c4835aabab20b4083eb7091db6
wuztb <bruckmayer@gmail.com>
2024-12-14 16:50:20 +0000

    demonstrated list

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
 import "./styles.css";
 
 export default function App() {
-  const pizzas = [
-    {
-      name: "Salami",
-      price: 12,
-    },
-    {
-      name: "Margharita",
-      price: 9,
-    },
-  ];
-
-  const pizzas2 = {
+  const pizzas = {
     Salami: {
       name: "Salami",
       price: 12,
@@ -23,12 +12,60 @@ export default function App() {
     },
   };
 
+  const pizzaData = [
+    {
+      name: "Focaccia",
+      ingredients: "Bread with italian olive oil and rosemary",
+      price: 6,
+      photoName: "pizzas/focaccia.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Margherita",
+      ingredients: "Tomato and mozarella",
+      price: 10,
+      photoName: "pizzas/margherita.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Spinaci",
+      ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+      price: 12,
+      photoName: "pizzas/spinaci.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Funghi",
+      ingredients: "Tomato, mozarella, mushrooms, and onion",
+      price: 12,
+      photoName: "pizzas/funghi.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Salamino",
+      ingredients: "Tomato, mozarella, and pepperoni",
+      price: 15,
+      photoName: "pizzas/salamino.jpg",
+      soldOut: true,
+    },
+    {
+      name: "Prosciutto",
+      ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+      price: 18,
+      photoName: "pizzas/prosciutto.jpg",
+      soldOut: false,
+    },
+  ];
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <Pizza pizza={pizzas.filter((p) => p.name === "Salami")[0]} />
-      <Pizza pizza={pizzas2["Margharita"]} />
+      <Pizza pizza={pizzas["Margharita"]} />
+      <Pizza pizza={pizzaData.filter((p) => p.name === "Prosciutto")[0]} />
+      {pizzaData.map((pizza) => (
+        <Pizza pizza={pizza} key={pizza.name} />
+      ))}
     </div>
   );
 }
