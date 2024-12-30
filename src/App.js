dfcd40fcc2e835cb1004ed073c2f380856671278
wuztb <bruckmayer@gmail.com>
2024-12-30 16:59:00 +0000

    load from json and seperate page

--- src/App.js
+++ src/App.js
@@ -1,76 +1,10 @@
+import FoodList from "./pages/FoodList";
 import "./styles.css";
 
 export default function App() {
-  const pizzas = {
-    Salami: {
-      name: "Salami",
-      price: 12,
-    },
-    Margharita: {
-      name: "Margharita",
-      price: 9,
-    },
-  };
-
-  const pizzaData = [
-    {
-      name: "Focaccia",
-      ingredients: "Bread with italian olive oil and rosemary",
-      price: 6,
-      photoName: "pizzas/focaccia.jpg",
-      soldOut: false,
-    },
-    {
-      name: "Margherita",
-      ingredients: "Tomato and mozarella",
-      price: 10,
-      photoName: "pizzas/margherita.jpg",
-      soldOut: false,
-    },
-    {
-      name: "Spinaci",
-      ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
-      price: 12,
-      photoName: "pizzas/spinaci.jpg",
-      soldOut: false,
-    },
-    {
-      name: "Funghi",
-      ingredients: "Tomato, mozarella, mushrooms, and onion",
-      price: 12,
-      photoName: "pizzas/funghi.jpg",
-      soldOut: false,
-    },
-    {
-      name: "Salamino",
-      ingredients: "Tomato, mozarella, and pepperoni",
-      price: 15,
-      photoName: "pizzas/salamino.jpg",
-      soldOut: true,
-    },
-    {
-      name: "Prosciutto",
-      ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
-      price: 18,
-      photoName: "pizzas/prosciutto.jpg",
-      soldOut: false,
-    },
-  ];
-
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <Pizza pizza={pizzas["Margharita"]} />
-      <Pizza pizza={pizzaData.filter((p) => p.name === "Prosciutto")[0]} />
-      {pizzaData.map((pizza) => (
-        <Pizza pizza={pizza} key={pizza.name} />
-      ))}
+      <FoodList />
     </div>
   );
-}
-
-function Pizza(props) {
-  console.log(props);
-  return <h1>This is a {props.pizza.name} Pizza</h1>;
 }
---
+++ src/pages/FoodList.js
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+
+function Pizza({ pizza }) {
+  //console.log(props);
+  return <p>This is a {pizza.name} Pizza</p>;
+}
+
+export default function FoodList() {
+  const [foodData, setFoodData] = useState();
+  const [pizzaData, setPizzaData] = useState();
+
+  async function loadFood() {
+    const response = await fetch("/data/food.json");
+    //console.log(response);
+    const data = await response.json();
+    //console.log(data);
+    setFoodData(data);
+  }
+
+  async function loadPizzas() {
+    const response = await fetch("/data/pizzas.json");
+    //console.log(response);
+    const data = await response.json();
+    //console.log(data);
+    setPizzaData(data);
+  }
+
+  useEffect(() => {
+    loadFood();
+    loadPizzas();
+  }, []);
+
+  return (
+    <div>
+      <h1>Use a key to access a pizza in pizzas.json</h1>
+      {/* need to ensure that pizzaData is loaded, therefore "pizzaData &&" is required if loading from file and not declaring variable  */}
+      {pizzaData && <Pizza pizza={pizzaData["Margharita"]} />}
+      <h1>Filter in food.json</h1>
+      {foodData && (
+        <>
+          <Pizza
+            pizza={foodData.pizzas.filter((p) => p.name === "Prosciutto")[0]}
+          />
+          {/* another option to achieve the same filter */}
+          <Pizza
+            pizza={foodData["pizzas"].filter((p) => p.name === "Prosciutto")[0]}
+          />
+        </>
+      )}
+      <h1>Loop through pizzas in food.json</h1>
+      {foodData &&
+        foodData.pizzas.map((pizza) => (
+          <Pizza pizza={pizza} key={pizza.name} />
+        ))}
+    </div>
+  );
+}
