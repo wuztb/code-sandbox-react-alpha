0cce5cd0b0d716971735503dc561f1e22b42d04c
wuztb <bruckmayer@gmail.com>
2024-12-31 07:05:06 +0000

    added extra page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import FoodList from "./pages/FoodList";
+import PizzaList from "./pages/PizzaList";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
+      <PizzaList />
       <FoodList />
     </div>
   );
---
+++ src/pages/PizzaList.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+
+function Pizza({ pizza }) {
+  //console.log(props);
+  return <p>This is a {pizza.name} Pizza</p>;
+}
+
+export default function PizzaList() {
+  const [pizzaData, setPizzaData] = useState();
+
+  async function loadPizzas() {
+    const response = await fetch("/data/pizzas.json");
+    //console.log(response);
+    const data = await response.json();
+    //console.log(data);
+    setPizzaData(data);
+  }
+
+  useEffect(() => {
+    loadPizzas();
+  }, []);
+
+  const markAsSoldOut = (pizzaName) => {};
+
+  const removeSoldOut = (pizzaName) => {};
+
+  return (
+    <div>
+      <h1>Use a key to access a pizza in pizzas.json</h1>
+      {/* need to ensure that pizzaData is loaded, therefore "pizzaData &&" is required if loading from file and not declaring variable  */}
+      {pizzaData && <Pizza pizza={pizzaData["Margharita"]} />}
+      <h1>Loop through pizzas in pizzas.json</h1>
+      {pizzaData &&
+        Object.entries(pizzaData).map(([key, pizza]) => (
+          <div
+            key={pizza.name}
+            className={pizza.status === "soldout" ? "soldout" : ""}
+          >
+            <Pizza pizza={pizza} />
+            {pizza.status === "soldout" ? (
+              <>
+                <p>{pizza.status}</p>
+                <button onClick={() => removeSoldOut(pizza.name)}>
+                  Remove Sold Out
+                </button>
+              </>
+            ) : (
+              <button onClick={() => markAsSoldOut(pizza.name)}>
+                Mark as Sold Out
+              </button>
+            )}
+          </div>
+        ))}
+    </div>
+  );
+}
