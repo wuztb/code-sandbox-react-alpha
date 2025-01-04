b57da14b78e66319d0e8e77a09777090af8b39eb
wuztb <bruckmayer@gmail.com>
2025-01-04 17:23:13 +0000

    added basic layout

--- src/App.js
+++ src/App.js
@@ -6,9 +6,26 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <PizzaList />
-      <FoodList />
-      <FoodOrder />
+      <div className="container">
+        <div className="header">
+          <h2>My Header</h2>
+        </div>
+        <div className="menu">
+          <a href="#">Link 1</a>
+          <br />
+          <a href="#">Link 2</a>
+          <br />
+          <a href="#">Link 3</a>
+        </div>
+        <div className="content">
+          <PizzaList />
+          <FoodList />
+          <FoodOrder />
+        </div>
+        <div className="footer">
+          <h4>Footer</h4>
+        </div>
+      </div>
     </div>
   );
 }
