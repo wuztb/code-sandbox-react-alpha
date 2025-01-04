9fa8da81495ae9ff2861d9572d128c84ebc2f2f0
wuztb <bruckmayer@gmail.com>
2025-01-04 17:55:55 +0000

    added basic layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import FoodList from "./pages/FoodList";
 import FoodOrder from "./pages/FoodOrder";
 import PizzaList from "./pages/PizzaList";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <PizzaList />
-      <FoodList />
-      <FoodOrder />
+      <div className="container">
+        <div className="header">
+          <h2>My Header</h2>
+        </div>
+        <div className="menu">
+          <a href="page1">Page 1 pizza.json</a>
+          <br />
+          <a href="page2">Page 2 food.json</a>
+          <br />
+          <a href="page3">Page 3 form fun</a>
+        </div>
+        <div className="content">
+          <Router>
+            <Routes>
+              <Route path="page1" element={<PizzaList />} />
+              <Route path="page2" element={<FoodList />} />
+              <Route path="page3" element={<FoodOrder />} />
+            </Routes>
+          </Router>
+        </div>
+        <div className="footer">
+          <h4>Footer</h4>
+        </div>
+      </div>
     </div>
   );
 }
