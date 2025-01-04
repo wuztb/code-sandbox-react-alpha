c105beb38047e076387a96b21ba1a797beb5c0ec
wuztb <bruckmayer@gmail.com>
2025-01-04 19:08:27 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import FoodList from "./pages/FoodList";
 import FoodOrder from "./pages/FoodOrder";
 import PizzaList from "./pages/PizzaList";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "./styles.css";
 
 export default function App() {
@@ -11,16 +12,20 @@ export default function App() {
           <h2>My Header</h2>
         </div>
         <div className="menu">
-          <a href="#">Link 1</a>
+          <a href="page1">Page 1 pizza.json</a>
           <br />
-          <a href="#">Link 2</a>
+          <a href="page2">Page 2 food.json</a>
           <br />
-          <a href="#">Link 3</a>
+          <a href="page3">Page 3 form fun</a>
         </div>
         <div className="content">
-          <PizzaList />
-          <FoodList />
-          <FoodOrder />
+          <Router>
+            <Routes>
+              <Route path="page1" element={<PizzaList />} />
+              <Route path="page2" element={<FoodList />} />
+              <Route path="page3" element={<FoodOrder />} />
+            </Routes>
+          </Router>
         </div>
         <div className="footer">
           <h4>Footer</h4>
