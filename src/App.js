ddfff780f7f87c3bef10ff74cd0c004e3e57e330
wuztb <bruckmayer@gmail.com>
2025-01-04 18:07:20 +0000

    fixed?

--- src/App.js
+++ src/App.js
@@ -34,35 +34,3 @@ export default function App() {
     </div>
   );
 }
-
-import FoodList from "./pages/FoodList";
-import FoodOrder from "./pages/FoodOrder";
-import PizzaList from "./pages/PizzaList";
-import "./styles.css";
-
-export default function App() {
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="header">
-          <h2>My Header</h2>
-        </div>
-        <div className="menu">
-          <a href="#">Link 1</a>
-          <br />
-          <a href="#">Link 2</a>
-          <br />
-          <a href="#">Link 3</a>
-        </div>
-        <div className="content">
-          <PizzaList />
-          <FoodList />
-          <FoodOrder />
-        </div>
-        <div className="footer">
-          <h4>Footer</h4>
-        </div>
-      </div>
-    </div>
-  );
-}
